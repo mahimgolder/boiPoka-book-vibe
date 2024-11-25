9b86534fcbe308b9b7cf39edcc2d6cd32233629b
mahimgolder <mahimgolder@mahim>
2024-11-26 01:48:43 +0600

    Setup rect router tailwind daisyUi, nested router and navbar section

---
+++ src/components/DashBoard/DashBoard.jsx
@@ -0,0 +1,10 @@
+
+const DashBoard = () => {
+    return (
+        <div>
+            <h1 className="text-5xl">DashBoard.jsx</h1>
+        </div>
+    );
+};
+
+export default DashBoard;
---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,10 @@
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1 className="text-5xl">Page not found </h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
